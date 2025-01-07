4668b72e948e53fde1936f5ea5fd307db77de623
GuilhermexL <guilhermesantosfv@gmail.com>
2025-01-07 13:17:34 -0300

    Enviando arquivo de Portfólio

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* Expressão para Texto Animado */
+
+let typingEffect = new Typed(".typedText",{
+  strings : ["Desenvolvedor."],
+  loop : true,
+  typeSpeed : 75, 
+  backSpeed : 100,
+  backDelay : 2000
+})
